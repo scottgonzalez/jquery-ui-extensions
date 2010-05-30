ddbd1e430c2e169abd6854f111a0198f5e08dc32
Scott González <scott.gonzalez@gmail.com>
2010-05-29 21:29:02 -0400

    Autocomplete: selectFirst option.

---
+++ autocomplete/jquery.ui.autocomplete.selectFirst.js
@@ -0,0 +1,15 @@
+(function() {
+
+$( ".ui-autocomplete-input" ).live( "autocompleteopen", function() {
+	var autocomplete = $( this ).data( "autocomplete" ),
+		menu = autocomplete.menu;
+
+	if ( !autocomplete.options.selectFirst ) {
+		return;
+	}
+
+	menu.activate( $.Event({ type: 'mouseenter' }), menu.element.children().first() );
+});
+
+})( jQuery );
+
