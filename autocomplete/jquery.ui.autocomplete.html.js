f85e922191893e3ef6bfd0843d7a2db4f81356f4
Scott González <scott.gonzalez@gmail.com>
2010-10-25 21:49:00 -0400

    Autocomplete (html option): Fixed searching so you can't match the content of the HTML tags.

--- autocomplete/jquery.ui.autocomplete.html.js
+++ autocomplete/jquery.ui.autocomplete.html.js
@@ -8,11 +8,33 @@
  */
 (function( $ ) {
 
-$.ui.autocomplete.prototype._renderItem = function( ul, item) {
-	return $( "<li></li>" )
-		.data( "item.autocomplete", item )
-		.append( $( "<a></a>" )[ this.options.html ? "html" : "text" ]( item.label ) )
-		.appendTo( ul );
-};
+var proto = $.ui.autocomplete.prototype,
+	initSource = proto._initSource;
+
+function filter( array, term ) {
+	var matcher = new RegExp( $.ui.autocomplete.escapeRegex(term), "i" );
+	return $.grep( array, function(value) {
+		return matcher.test( $( "<div>" ).html( value.label || value.value || value ).text() );
+	});
+}
+
+$.extend( proto, {
+	_initSource: function() {
+		if ( $.isArray(this.options.source) ) {
+			this.source = function( request, response ) {
+				response( filter( this.options.source, request.term ) );
+			};
+		} else {
+			initSource.call( this );
+		}
+	},
+
+	_renderItem: function( ul, item) {
+		return $( "<li></li>" )
+			.data( "item.autocomplete", item )
+			.append( $( "<a></a>" )[ this.options.html ? "html" : "text" ]( item.label ) )
+			.appendTo( ul );
+	}
+});
 
 })( jQuery );
