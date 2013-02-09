e7c05b704f8b62567f310f81da0221b1b689df8c
Scott González <scott.gonzalez@gmail.com>
2013-02-08 20:46:43 -0500

    Whitespace.

--- autocomplete/demo.js
+++ autocomplete/demo.js
@@ -2,7 +2,7 @@ $(function() {
 
 var sourceView = $( "<div></div>", {
 		"class": "source"
-	}) 
+	})
 	.appendTo( "body" );
 
 $( "<p>This demo is powered by the following code:</p>").appendTo( sourceView );
@@ -13,13 +13,13 @@ $( "<p>Look at the source option to see the possible values.</p>")
 $("script:not([src])").each(function() {
 	var lines = $( this ).html().split( "\n" );
 	lines.shift();
-	
+
 	// unindent by one tab
 	// join on \r\n instead of just \n for IE
 	var src = $.map( lines, function( line ) {
 		return line.substring(1);
 	}).join( "\r\n" );
-	
+
 	// append source to the page
 	$( "<pre></pre>" )
 		.text( src )
--- dialog/jquery.ui.dialog.autoReposition.js
+++ dialog/jquery.ui.dialog.autoReposition.js
@@ -10,12 +10,12 @@
 
 $.ui.dialog.prototype.options.autoReposition = true;
 $( window ).resize(function() {
-    $( ".ui-dialog-content:visible" ).each(function() {
-        var dialog = $( this ).data( "dialog" );
-        if ( dialog.options.autoReposition ) {
-            dialog.option( "position", dialog.options.position );
-        }
-    });
+	$( ".ui-dialog-content:visible" ).each(function() {
+		var dialog = $( this ).data( "dialog" );
+		if ( dialog.options.autoReposition ) {
+			dialog.option( "position", dialog.options.position );
+		}
+	});
 });
 
 })( jQuery );
